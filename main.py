3f2c914cdf239995b7e088e21fdbbf4c0790872b
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-04-10 22:58:44 +0300

    Working with class_ = user__name

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ soup = BeautifulSoup(src, 'html.parser')
 
 # .find() or find_all()
 
-page_h1_all = soup.find_all("h1")
-print(page_h1_all)
+# page_h1_all = soup.find_all("h1")
+# print(page_h1_all)
+#
+# for item in page_h1_all:
+#     print(item.text)
 
-for item in page_h1_all:
-    print(item.text)
-
+user_name = soup.find("div", class_="user__name")
+# user_name = soup.find("span")
+print(user_name.text.strip())
