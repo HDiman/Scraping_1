b77c6f52204849ad492ba4e62c680f723e9eee3f
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-04-10 22:52:26 +0300

    Working with h1

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ with open("index.html") as file:
 
 soup = BeautifulSoup(src, 'html.parser')
 
-title = soup.title.string
-print(title)
+# title = soup.title.string
+# print(title)
+
+# .find() or find_all()
+
+page_h1_all = soup.find_all("h1")
+print(page_h1_all)
+
+for item in page_h1_all:
+    print(item.text)
+
